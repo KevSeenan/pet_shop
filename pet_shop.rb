dd43c163c2016341efd524bcd32de0ebcda1bc85
KevSeenan <kev.seenan@gmail.com>
2019-07-19 13:35:19 +0100

    commit for total cash - updated

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,12 +1,12 @@
-#(1)
-def pet_shop_name(shop)
-  return shop[:name]
-end
-
-#(2)
-# def total_cash(shop)
-#   return @pet_shop[:admin][:total_cash]
+# #(1)
+# def pet_shop_name(shop)
+#   return shop[:name]
 # end
+
+(2)
+def total_cash(shop)
+  return shop[:admin][:total_cash]
+end
 
 #(3)
 # def add_or_remove_cash(shop, cash, amount)
